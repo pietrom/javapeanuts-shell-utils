3e378cd0a292baa2b6c470ead427427a5325f746
Pietro Martinelli <amicofragile@gmail.com>
2012-04-12 11:13:28 +0200

    Added script that look for a class-name pattern traversing JAR files contained in current directory or its subdirectories

---
+++ find-jar.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+jarsFound=""
+for file in $(find . -name "*.jar"); do
+	echo "Processing file ${file} ..."
+        out=$(jar tf ${file} | grep ${1})
+	if [ "${out}" != "" ]
+	then
+		echo "  Found '${1}' in JAR file ${file}"
+		jarsFound="${jarsFound} ${file}"
+	fi
+done
+echo "${jarsFound}"
+
+echo ""
+echo "Search result:"
+echo "" 
+
+if [ "${jarsFound}" != "" ]
+then
+	echo "${1} found in"
+	for file in ${jarsFound}
+	do
+		echo "- ${file}"
+	done
+else
+	echo "${1} not found"
+fi
